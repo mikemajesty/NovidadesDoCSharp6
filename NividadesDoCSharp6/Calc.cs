98417f492855f6a6704540a181ee0023c8f981ec
mikemajesty <mikemajesty2011@gmail.com>
2015-12-14 18:38:16 -0200

    Aula 01 - Using Static

---
+++ NividadesDoCSharp6/Calc.cs
@@ -0,0 +1,8 @@
+﻿namespace NividadesDoCSharp6.Util
+{
+    public class Calc
+    {
+        public static int Calcular(int x, int y) => x + y; 
+
+    }
+}
---
+++ NividadesDoCSharp6/Program.cs
@@ -0,0 +1,16 @@
+﻿using static NividadesDoCSharp6.Util.Calc;
+using static System.Console;
+
+namespace NividadesDoCSharp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = Calcular(2,2);
+            WriteLine(r);
+            ReadKey();
+        }
+    }
+   
+}
