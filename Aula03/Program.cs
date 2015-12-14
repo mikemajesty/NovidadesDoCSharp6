50603cdb9975b6a10a83b0aeaec730557a51237b
mikemajesty <mikemajesty2011@gmail.com>
2015-12-14 21:30:04 -0200

    Aula 03 - Auto Property Initializer

---
+++ Aula03/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace Aula03
+{
+    public class Aluno
+    {
+
+        public int Matricula { get; set; } = 333;
+        public string Nome { get; set; } = "Limão";
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var aluno = new Aluno();
+            WriteLine($"Nome: {aluno.Nome} Matricula: {aluno.Matricula}");
+            ReadKey();
+        }
+    }
+}
