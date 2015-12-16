62cc3c79c0037e431bde09f4813d9c6c7bbf8db9
mikemajesty <mikemajesty2011@gmail.com>
2015-12-16 09:03:13 -0200

    Aula 07 - Exceptions Filters

---
+++ Aula07/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+namespace Aula07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 999999999;
+            int y = 999999999;
+            try
+            {
+                int r1 = x / y;
+                int r2 = checked(x * y * x);
+                WriteLine($"{x} dividido por {y} e igual a {r1}");
+                WriteLine($"{x} multiplicado por {y} e igual a {r2}");
+            }               
+            catch (Exception erro) when (erro.Message.ToLower().Contains("divide"))
+            {
+                WriteLine($"Error:{erro.Message}");
+            }
+            catch (Exception erro) when (erro.Message.ToLower().Contains("overflow"))
+            {
+                WriteLine($"Error:{erro.Message}");
+            }
+            ReadKey();
+
+        }
+
+    }
+}
