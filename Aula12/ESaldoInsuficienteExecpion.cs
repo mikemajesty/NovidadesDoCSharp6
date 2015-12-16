8acae2f597a5755b5c8771b0bf6821d5b0ce37d8
mikemajesty <mikemajesty2011@gmail.com>
2015-12-16 12:44:20 -0200

    Aula 12 - Gerando classes de Exceptions

---
+++ Aula12/ESaldoInsuficienteExecpion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Aula12
+{
+    [Serializable]
+    public class ESaldoInsuficienteExecpion : Exception
+    {
+        public ESaldoInsuficienteExecpion()
+        {
+        }
+
+        public ESaldoInsuficienteExecpion(string message) : base(message)
+        {
+        }
+
+        public ESaldoInsuficienteExecpion(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ESaldoInsuficienteExecpion(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ Aula12/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace Aula12
+{
+    class Program
+    {
+        public class Conta
+        {
+            private double _saldo;
+            private const double LIMITE = 500;
+            public double GetSaldo() => _saldo;
+
+            public void Desositar(double valor)
+            {
+                _saldo += valor;
+            }
+            public void Sacar(double valor)
+            {
+                if (valor > _saldo + LIMITE)
+                    throw new ESaldoInsuficienteExecpion("Saldo Insuficiente") ;              
+                    _saldo -= valor;
+              
+            }
+        }
+        static void Main(string[] args)
+        {
+
+
+            WriteLine("Informe o valor a Depositar");
+            var v1 = Convert.ToDouble(ReadLine());
+            WriteLine("Informe o valor a Sacar");
+            var v2 = Convert.ToDouble(ReadLine());
+            var conta = new Conta();
+            conta.Desositar(v1);
+            conta.Sacar(v2);
+           
+            WriteLine($"Sua conta em saldo de {conta.GetSaldo()}");
+            ReadKey();
+        }
+
+
+    }
+}
